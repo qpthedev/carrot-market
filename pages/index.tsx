7da2ebd05f4da2c4082e7ae9a647e51f3e489847
qpthedev <qpthedev@gmail.com>
2022-02-23 02:09:20 -0500

    Tailwind CSS practice - Modifiers, Transitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,98 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between">
+          <span className="text-gray-500 my-2">Gray Chair</span>
+          <span className="font-semibold">$15</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500 ">Gray Chair</span>
+          <span className="font-semibold">$15</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$30</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          Checkout
+        </div>
+      </div>
+
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+
+        <div className="bg-white rounded-3xl p-6 relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between ">
+            <div className="flex flex-col items-center ">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+
+            <div className="flex flex-col items-center ">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+
+          <div className="relative flex flex-col items-center -mt-10 -mb-15 ">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <div className="flex mb-5 justify-between items-center ">
+          <span>⬅️</span>
+          <div className="space-x-2">
+            <span>⭐ 4.9</span>
+            <span className="shadow-xl p-2 rounded-md">💖</span>
+          </div>
+        </div>
+
+        <div className="bg-zinc-400 h-72 mb-5" />
+
+        <div className="flex flex-col">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500 ">Chair</span>
+
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
+            </div>
+
+            <div className="flex items-center space-x-5">
+              <button className="bg-blue-200 flex justify-center items-center aspect-square w-10 text-xl text-gray-500 rounded-lg">
+                +
+              </button>
+              <span>1</span>
+              <button className="bg-blue-200 flex justify-center items-center aspect-square w-10 text-xl text-gray-500 rounded-lg">
+                -
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 text-center text-white rounded-lg py-4 px-8">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
+  );
 };
 
 export default Home;
