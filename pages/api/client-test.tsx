d63b8cff84639181c045614621beb8624d83bba5
qpthedev <qpthedev@gmail.com>
2022-03-21 03:42:01 -0400

    Prisma client base setup

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {}
