93ebab1c6bc9eb672ec82b9614be94d0c9e8d0cd
qpthedev <qpthedev@gmail.com>
2022-04-12 03:09:27 -0400

    Prisma schema update - token

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -46,18 +46,35 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   // }
   const payload = phone ? { phone: +phone } : { email };
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  // const user = await client.user.upsert({
+  //   where: {
+  //     ...payload,
+  //   },
+  //   create: {
+  //     name: "Anonymous",
+  //     ...payload,
+  //   },
+  //   update: {},
+  // });
+
+  const token = await client.token.create({
+    data: {
+      payload: "12345",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
 
-  console.log(user);
+  console.log(token);
 
   return res.status(200).end();
 }
