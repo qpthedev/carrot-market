847c6220e1b4aa144e34c29f7405f1149dd73cf0
qpthedev <qpthedev@gmail.com>
2022-04-12 02:27:21 -0400

    user account check logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,62 @@ import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) {
+  //     console.log("User Found");
+  //   }
+  //   if (!user) {
+  //     console.log("Creating a new user");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   }
+  // }
+
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) {
+  //     console.log("User Found");
+  //   }
+  //   if (!user) {
+  //     console.log("Creating a new user");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  // }
+  const payload = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
+  console.log(user);
+
   return res.status(200).end();
 }
 
