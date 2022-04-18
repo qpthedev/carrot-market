5adfa4f9ba24692502a7ca276d4102c714997203
qpthedev <qpthedev@gmail.com>
2022-04-18 01:01:13 -0400

    Set up Twilio test acct for SMS token validation

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -28,6 +31,15 @@ async function handler(
       },
     },
   });
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.PHONE_NUMBER!,
+      body: `Your log in token is ${payload}`,
+    });
+    console.log(message);
+  }
 
   return res.json({
     ok: true,
