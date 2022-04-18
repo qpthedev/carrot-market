f019a8412e8cb97d02528d46c2f075edfdeb84fa
qpthedev <qpthedev@gmail.com>
2022-04-18 00:13:00 -0400

    refactor user enter api

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,82 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
 
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (user) {
-  //     console.log("User Found");
-  //   }
-  //   if (!user) {
-  //     console.log("Creating a new user");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         email,
-  //       },
-  //     });
-  //   }
-  // }
+  if (!user) return res.status(400).json({ ok: false });
 
-  // if (phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //   });
-  //   if (user) {
-  //     console.log("User Found");
-  //   }
-  //   if (!user) {
-  //     console.log("Creating a new user");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         phone: +phone,
-  //       },
-  //     });
-  //   }
-  // }
-  const payload = phone ? { phone: +phone } : { email };
-
-  // const user = await client.user.upsert({
-  //   where: {
-  //     ...payload,
-  //   },
-  //   create: {
-  //     name: "Anonymous",
-  //     ...payload,
-  //   },
-  //   update: {},
-  // });
-
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
-      payload: "12345",
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: "Anonymous",
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
 
-  console.log(token);
-
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
